812e96883d24b483cd329415480af641531f591c
williamzhang11 <1107517306@qq.com>
2019-08-04 17:48:56 +0800

    first commit

---
+++ src/main/java/com/xiu/fastarchdegin/App.java
@@ -0,0 +1,13 @@
+package com.xiu.fastarchdegin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
